d9cb8d72399b6d1fa02a0c18233146b6dc225fca
liuyangyangwork <liuyangyangwork@163.com>
2017-05-16 15:32:41 +0800

    config

--- a.js
+++ a.js
@@ -8,11 +8,12 @@ define(function(){
         aFn:aFn
     }
 })
-// define(['a'],function(){
+// define(['a'],function(bM){
 //     console.log('a')
 //     function aFn(){
 //         console.log("我是a模块函数！")
 //     }
+//bM.bFn
 //     //提供接口
 //     return{
 //         aFn:aFn
